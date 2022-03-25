40f184b050aa0094031cfe71889933f1e7cc5609
Jake Dunafon <jlad795@gmail.com>
2022-03-24 22:19:18 -0700

    Update CSS

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,7 @@ import '../Movie.css'
 const Movie = (props) => {
         return (
             <article className='movie-card'>
-                <h3>{props.title}</h3>
+                {/* <h3>{props.title}</h3> */}
                 <img src={props.poster_path} />
                 <h4>Rating: {Math.round(props.average_rating*100)/100}</h4>
             </article>
